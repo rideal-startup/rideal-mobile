130eb43f29f8639c83fb28acaec11bd436bc1d27
Guillem96 <guillem.orellana@gmail.com>
2019-10-22 18:15:59 +0200

    i18n

---
+++ lib/services/i18n.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:yaml/yaml.dart';
+
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  // Helper method to keep the code in the widgets concise
+  // Localizations are accessed using an InheritedWidget "of" syntax
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  Map<String, String> _localizedStrings;
+
+  Future<bool> load() async {
+    // Load the language JSON file from the "lang" folder
+    String ymlString =
+        await rootBundle.loadString('assets/i18n/${locale.languageCode}.yml');
+    YamlMap translations = loadYaml(ymlString);
+    _localizedStrings = translations.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+    
+
+    return true;
+  }
+
+  String translate(String key, [Map<String, String> params]) {
+    var str = _localizedStrings[key];
+    for (var e in params.entries)
+      str.replaceAll('{{${e.key}}}', e.value);
+    return str;
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    // Include all of your supported language codes here
+    return ['en', 'es'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    // AppLocalizations class is where the JSON loading actually runs
+    AppLocalizations localizations = new AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
