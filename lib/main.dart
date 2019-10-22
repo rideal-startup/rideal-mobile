70396830a404e1243ab877165ab92c200b563b41
MrceL97 <9719marcel@gmail.com>
2019-10-22 20:52:37 +0200

    Loading screen reubicated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:rideal/widgets/Loading.dart';
+import 'package:rideal/screens/home/login.dart';
 import 'package:rideal/services/i18n.dart';
-import 'package:rideal/theme/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:rideal/widgets/Navbar.dart';
 
--- lib/screens/home/login.dart
+++ lib/screens/home/login.dart
@@ -1,11 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:rideal/services/i18n.dart';
-
-import '../utils.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key key}) : super(key: key);
